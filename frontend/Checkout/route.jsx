cf4f311f49baf4c98e6579eb7f77ad6908e04dc5
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-04-13 17:13:19 +0200

    PWA-151 Add Checkout page to FE

---
+++ frontend/Checkout/route.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Route from '@shopgate/pwa-common/components/Router/components/Route'
+import Checkout from './index'
+
+const CheckoutRoute = (props) => (
+  <Route
+    path='/checkout2'
+    component={Checkout}
+    {...props}
+  />
+)
+
+export default CheckoutRoute
