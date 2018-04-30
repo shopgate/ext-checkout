b02518bf4840050f7a93255445102901eea35314
rutkowski <waldemar.rutkowski@shopgate.com>
2018-04-30 16:31:16 +0200

    PWA-199: Added an empty validation pipeline-step to the checkout pipeline for custom validation

---
+++ extension/checkout/validateCheckout.js
@@ -0,0 +1,10 @@
+/**
+ * No validation by default
+ *
+ * @param {SDKContext} context
+ * @param {Object} input
+ * @param {ExtCheckout} input.checkout
+ * @returns {Promise<null>}
+ */
+module.exports = async (context, input) => {
+}
