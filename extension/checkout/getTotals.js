efd3973130961d6cb45b89389d3e485ad675acea
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-05-18 15:01:46 +0200

    PWA-401 Get checkout totals pipeline and processing

--- extension/checkout/getTotals.js
+++ extension/checkout/getTotals.js
@@ -20,6 +20,7 @@
 module.exports = async (context, input) => {
   let taxAmount = 0
   let total = 0
+  const totals = []
 
   total += input.items
     .map(item => item.unitPrice * item.quantity)
@@ -28,19 +29,66 @@ module.exports = async (context, input) => {
       0
     )
 
+  totals.push({
+    id: 'subtotal',
+    label: 'Subtotal',
+    amount: total
+  })
+
+  const coupon = input.items
+    .filter(item => item.type === 'coupon')
+    .map(item => item.unitPrice * item.quantity)
+    .reduce(
+      (sum, amount) => sum + amount,
+      0
+    )
+
+  if (coupon) {
+    totals.push({
+      id: 'coupon',
+      label: 'Coupon',
+      amount: coupon
+    })
+  }
+
   if (input.shippingMethod) {
     total += input.shippingMethod.amount
     taxAmount += input.shippingMethod.taxAmount
+
+    totals.push({
+      id: 'shipping',
+      label: 'Shipping',
+      amount: input.shippingMethod.amount
+    })
   }
 
   if (input.paymentMethod) {
     total += input.paymentMethod.amount
     taxAmount += input.paymentMethod.taxAmount
+
+    totals.push({
+      id: 'payment',
+      label: 'Payment',
+      amount: input.paymentMethod.amount
+    })
   }
+
+  total += taxAmount
+
+  totals.push({
+    id: 'tax',
+    label: 'Tax',
+    amount: taxAmount
+  }, {
+    id: 'total',
+    label: 'Total',
+    amount: total
+  })
 
   return {
     currency: context.config.currency,
     taxAmount,
-    total
+    total,
+    totals
   }
 }
--- extension/test/unit/checkout/getTotals-test.js
+++ extension/test/unit/checkout/getTotals-test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
-const executeStep = require('../../../checkout/calculateTotals')
+const executeStep = require('../../../checkout/getTotals')
 
-describe('calculateTotals', () => {
+describe('getTotals', () => {
   /** @type {ExtCheckoutItem[]} */
   const items = [
     {id: 'SG1', name: 'Cool product', type: 'product', unitPrice: 500, quantity: 2},
@@ -16,7 +16,15 @@ describe('calculateTotals', () => {
     const expectedTotals = {
       currency: 'EUR',
       taxAmount: 19,
-      total: 1090
+      total: 1109,
+      totals: [
+        {id: 'subtotal', amount: 900, label: 'Subtotal'},
+        {id: 'coupon', amount: -100, label: 'Coupon'},
+        {id: 'shipping', amount: 90, label: 'Shipping'},
+        {id: 'payment', amount: 100, label: 'Payment'},
+        {id: 'tax', amount: 19, label: 'Tax'},
+        {id: 'total', amount: 1109, label: 'Total'}
+      ]
     }
     let stepError, totals
     try {
@@ -38,7 +46,14 @@ describe('calculateTotals', () => {
     const expectedTotals = {
       currency: 'EUR',
       taxAmount: 9,
-      total: 990
+      total: 999,
+      totals: [
+        {id: 'subtotal', amount: 900, label: 'Subtotal'},
+        {id: 'coupon', amount: -100, label: 'Coupon'},
+        {id: 'shipping', amount: 90, label: 'Shipping'},
+        {id: 'tax', amount: 9, label: 'Tax'},
+        {id: 'total', amount: 999, label: 'Total'}
+      ]
     }
     let stepError, totals
     try {
@@ -56,7 +71,14 @@ describe('calculateTotals', () => {
     const expectedTotals = {
       currency: 'EUR',
       taxAmount: 10,
-      total: 1000
+      total: 1010,
+      totals: [
+        {id: 'subtotal', amount: 900, label: 'Subtotal'},
+        {id: 'coupon', amount: -100, label: 'Coupon'},
+        {id: 'payment', amount: 100, label: 'Payment'},
+        {id: 'tax', amount: 10, label: 'Tax'},
+        {id: 'total', amount: 1010, label: 'Total'}
+      ]
     }
     let stepError, totals
     try {
