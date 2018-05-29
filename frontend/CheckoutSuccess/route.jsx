09dd2f54a457dd948b69fa88eafc121bc0b9ef0e
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-05-29 13:20:12 +0200

    PWA-401 Append user data

--- frontend/CheckoutSuccess/route.jsx
+++ frontend/CheckoutSuccess/route.jsx
@@ -3,6 +3,11 @@ import Route from '@shopgate/pwa-common/components/Router/components/Route';
 import AuthRoutes from '@shopgate/pwa-common/components/Router/components/AuthRoutes';
 import CheckoutSuccess from './index';
 
+/**
+ * @param {Object} props props
+ * @return {*}
+ * @constructor
+ */
 const CheckoutSuccessRoute = props => (
   <AuthRoutes to="/login">
     <Route
