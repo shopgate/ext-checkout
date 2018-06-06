839bc9060a8c711cb9a03930f2341ed5b07619cd
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2018-06-06 13:58:58 +0200

    PWA-495: Fixed showing up totals, since the getTotals pipeline does not return a currency anymore, which is required for the frontend.

--- frontend/Checkout/components/Totals/index.jsx
+++ frontend/Checkout/components/Totals/index.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { App } from '@shopgate/pwa-common/context';
 import Grid from '@shopgate/pwa-common/components/Grid';
 import I18n from '@shopgate/pwa-common/components/I18n';
 import connect from './connector';
@@ -9,21 +10,25 @@ import { textRight } from './style';
  * @param {Object} props props
  * @return {*}
  */
-const Totals = ({ currency, totals }) => (
-  <Fragment>
-    {
-      totals.map(total => (
-        <Fragment key={total.id}>
-          <Grid>
-            <Grid.Item grow={1}>{total.label}</Grid.Item>
-            <Grid.Item grow={999} schrink={0} className={textRight}>
-              <I18n.Price price={total.amount} currency={currency} />
-            </Grid.Item>
-          </Grid>
-        </Fragment>
-      ))
-    }
-  </Fragment>
+const Totals = ({ totals }) => (
+  <App>
+    {({ checkout }) => (
+      <Fragment>
+        {
+          totals.map(total => (
+            <Fragment key={total.id}>
+              <Grid>
+                <Grid.Item grow={1}>{total.label}</Grid.Item>
+                <Grid.Item grow={999} schrink={0} className={textRight}>
+                  <I18n.Price price={total.amount} currency={checkout.currency} />
+                </Grid.Item>
+              </Grid>
+            </Fragment>
+          ))
+        }
+      </Fragment>
+    )}
+  </App>
 );
 
 Totals.propTypes = {
